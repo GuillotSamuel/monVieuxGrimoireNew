09eccb13c57ccce9e3af8900ce32f98642ad7f4c
GuillotSamuel <samuel.guillot75@gmail.com>
2023-07-12 19:08:24 +0200

    get the 3 best rated books

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -30,13 +30,19 @@ exports.getOneBook = async (req, res) => {
   }
 };
 
-/* KO Get the 3 best rated books */
-exports.getBestRatings = (req, res, next) => {
-  Book.find()
-    .sort({ averageRating: -1 })
-    .limit(3)
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
+/* Get the 3 best rated books */
+exports.getBestRatings = async (req, res) => {
+  try {
+    const books = await Book.find()
+      .sort({ averageRating: -1 })
+      .limit(3)
+      .then((books) => res.status(200).json(books))
+      .catch((error) => res.status(400).json({ error }));
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
 };
 
 /* Post a new book */
@@ -64,75 +70,91 @@ exports.postBook = (req, res, next) => {
 
 /* KO Update one book datas */
 
+/* exports.modifyBook = async (req, res, next) => {
+  try {
+    const book = await Book.findOne({ _id: req.params.id });
+
+    if (book.userId !== req.auth.userId) {
+      return res
+        .status(403)
+        .json({
+          message: "User not authorized to modify books created by others",
+        });
+    }
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+}; */
+
 /* Delete one book */
-exports.deleteBook = (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => {
-      if (book.userId !== req.auth.userId) {
-        return res.status(403).json({ message: "Not authorized" });
+exports.deleteBook = async (req, res, next) => {
+  try {
+    const book = await Book.findOne({ _id: req.params.id });
+
+    if (book.userId !== req.auth.userId) {
+      return res.status(403).json({ message: "Not authorized" });
+    }
+
+    const filename = book.imageUrl.split("/images/")[1];
+    fs.unlink(`images/${filename}`, (error) => {
+      if (error) {
+        return res.status(500).json({ error });
       }
 
-      const filename = book.imageUrl.split("/images/")[1];
-      fs.unlink(`images/${filename}`, (error) => {
-        if (error) {
-          return res.status(500).json({ error });
-        }
-
-        Book.deleteOne({ _id: req.params.id })
-          .then(() => {
-            res.status(200).json({
-              message: "Book deleted",
-            });
-          })
-          .catch((error) => {
-            res.status(500).json({ error });
+      Book.deleteOne({ _id: req.params.id })
+        .then(() => {
+          res.status(200).json({
+            message: "Book deleted",
           });
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
+        })
+        .catch((error) => {
+          res.status(500).json({ error });
+        });
     });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
-/* KO Post a rating of one book */
+/* Post a rating of one book */
 
-exports.postRatingBook = (req, res, next) => {
-  const url = req.url;
-  const urlId = url.split("/")[1];
-  const bookFilter = { _id: urlId };
-  const updatedUserId = req.body.userId;
-  const updatedGrade = req.body.rating;
+exports.postRatingBook = async (req, res, next) => {
+  try {
+    if (req.body.rating >= 0 && req.body.rating <= 5) {
+      const newRating = {
+        userId: req.body.userId,
+        grade: req.body.rating,
+      };
 
-  const updatedData = {
-    userId: updatedUserId,
-    grade: updatedGrade,
-  };
+      const book = await Book.findOne({ _id: req.params.id });
 
-  Book.findOne(bookFilter)
-    .then((book) => {
-      if (book.ratings.some((rating) => rating.userId === updatedUserId)) {
+      if (book.ratings.includes(req.body.userId)) {
         return res.status(400).json({
-          error: "The user has already rated this book",
+          message: "The user has already rated this book.",
         });
       }
-      return Book.findOneAndUpdate(
-        bookFilter,
-        { $push: { ratings: updatedData } },
-        { new: true }
-      );
-    })
-    .then((updatedBook) => {
-      const totalRatings = updatedBook.ratings.length;
-      const ratingsSum = updatedBook.ratings.reduce(
-        (acc, rating) => acc + rating.grade,
+
+      book.ratings.push(newRating);
+      const gradeArray = book.ratings.map((grade) => grade.grade);
+      const sum = gradeArray.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
         0
       );
-      updatedBook.averageRating = (ratingsSum / totalRatings).toFixed(0);
+      const averageRating = sum / gradeArray.length;
+      book.averageRating = Math.round(averageRating);
 
-      return updatedBook.save();
-    })
-    .then((book) => {
-      res.status(200).json(book);
-    })
-    .catch((error) => res.status(400).json({ error }));
+      await book.save();
+
+      return res.status(201).json(book);
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Invalid rating. Rating must be between 0 and 5." });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -8,8 +8,8 @@ const sharp = require('../middleware/sharp');
 const booksCtrl = require("../controllers/book");
 
 router.get("/api/books/", booksCtrl.getAllBooks); // Get all books
-router.get("/api/books/:id", booksCtrl.getOneBook); // Get one book based on its id
 router.get("/api/books/bestrating", booksCtrl.getBestRatings); // Get the 3 best rated books
+router.get("/api/books/:id", booksCtrl.getOneBook); // Get one book based on its id
 router.post("/api/books/", auth, multer,/*  sharp, */ booksCtrl.postBook); // Post a new book
 /* router.put("/api/books/:id", auth, multer, sharp, booksCtrl.modifyBook); // Update one book datas
  */router.delete("/api/books/:id", auth, booksCtrl.deleteBook); // Delete one book
