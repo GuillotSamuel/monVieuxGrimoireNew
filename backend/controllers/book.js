a1ada5ebbaf0d8dffab03913c6dbab5aae0a3e7a
GuillotSamuel <samuel.guillot75@gmail.com>
2023-07-12 12:20:25 +0200

    supprimer un book

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,4 +1,5 @@
 const Book = require("../models/book");
+const fs = require("fs");
 
 /* Get all books */
 exports.getAllBooks = async (req, res) => {
@@ -29,60 +30,109 @@ exports.getOneBook = async (req, res) => {
   }
 };
 
-/* Get the 3 best rated books */
-exports.bestRating = (req, res , next) => {
+/* KO Get the 3 best rated books */
+exports.getBestRatings = (req, res, next) => {
   Book.find()
-    .sort({ averageRating : 'desc'})
-    .then((books) => res.status(200).json(books.splice(0, 3)))
-    .catch((error) => res.status(400).json({error}))
-}
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 /* Post a new book */
-exports.createBook = (req, res, next) => {
+exports.postBook = (req, res, next) => {
   const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
   delete bookObject._userId;
   const book = new Book({
-      ...bookObject,
-      userId: req.auth.userId,
-      imageUrl: req.file
-          ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
-          : null,
+    ...bookObject,
+    userId: req.auth.userId,
+    imageUrl: req.file
+      ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+      : null,
   });
 
-  book.save()
-      .then(() => {
-          res.status(201).json({ message: "Objet enregistré !" });
-      })
-      .catch((error) => {
-          res.status(400).json({ error });
-      });
+  book
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "New book saved" });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 };
 
-/* Update one book datas */
+/* KO Update one book datas */
 
 /* Delete one book */
 exports.deleteBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((book) => {
-      if (book.userId != req.auth.userId) {
-        res.status(403).json({ message: "Not authorized" });
-      } else {
-        const filename = book.imageUrl.split("/images/")[1];
-        fs.unlink(`images/${filename}`, () => {
-          Book.deleteOne({ _id: req.params.id })
-            .then(() => {
-              res.status(200).json({
-                message: "Objet supprimé !",
-              });
-            })
-            .catch((error) => res.status(401).json({ error }));
-        });
+      if (book.userId !== req.auth.userId) {
+        return res.status(403).json({ message: "Not authorized" });
       }
+
+      const filename = book.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, (error) => {
+        if (error) {
+          return res.status(500).json({ error });
+        }
+
+        Book.deleteOne({ _id: req.params.id })
+          .then(() => {
+            res.status(200).json({
+              message: "Book deleted",
+            });
+          })
+          .catch((error) => {
+            res.status(500).json({ error });
+          });
+      });
     })
     .catch((error) => {
       res.status(500).json({ error });
     });
 };
 
-/* Post a rating of one book */
+/* KO Post a rating of one book */
+
+exports.postRatingBook = (req, res, next) => {
+  const url = req.url;
+  const urlId = url.split("/")[1];
+  const bookFilter = { _id: urlId };
+  const updatedUserId = req.body.userId;
+  const updatedGrade = req.body.rating;
+
+  const updatedData = {
+    userId: updatedUserId,
+    grade: updatedGrade,
+  };
+
+  Book.findOne(bookFilter)
+    .then((book) => {
+      if (book.ratings.some((rating) => rating.userId === updatedUserId)) {
+        return res.status(400).json({
+          error: "The user has already rated this book",
+        });
+      }
+      return Book.findOneAndUpdate(
+        bookFilter,
+        { $push: { ratings: updatedData } },
+        { new: true }
+      );
+    })
+    .then((updatedBook) => {
+      const totalRatings = updatedBook.ratings.length;
+      const ratingsSum = updatedBook.ratings.reduce(
+        (acc, rating) => acc + rating.grade,
+        0
+      );
+      updatedBook.averageRating = (ratingsSum / totalRatings).toFixed(0);
+
+      return updatedBook.save();
+    })
+    .then((book) => {
+      res.status(200).json(book);
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -9,10 +9,10 @@ const booksCtrl = require("../controllers/book");
 
 router.get("/api/books/", booksCtrl.getAllBooks); // Get all books
 router.get("/api/books/:id", booksCtrl.getOneBook); // Get one book based on its id
-router.get("/api/books/bestrating", booksCtrl.bestRating); // Get the 3 best rated books
-router.post("/api/books/", auth, multer,/*  sharp, */ booksCtrl.createBook); // Post a new book
+router.get("/api/books/bestrating", booksCtrl.getBestRatings); // Get the 3 best rated books
+router.post("/api/books/", auth, multer,/*  sharp, */ booksCtrl.postBook); // Post a new book
 /* router.put("/api/books/:id", auth, multer, sharp, booksCtrl.modifyBook); // Update one book datas
  */router.delete("/api/books/:id", auth, booksCtrl.deleteBook); // Delete one book
-/* router.post("/api/books/:id/rating", auth, booksCtrl.ratingBook); // Post a rating of one book
- */
+router.post("/api/books/:id/rating", auth, booksCtrl.postRatingBook); // Post a rating of one book
+
 module.exports = router;
